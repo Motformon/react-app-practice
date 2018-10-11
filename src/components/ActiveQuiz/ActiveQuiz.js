6b5b95577ed7e5b08901cec419dcc01373c2748d
Motformon <Motformon@gmail.com>
2018-10-11 09:01:28 +0300

    Создание и ращметка проекта

---
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import classes from "./ActiveQuiz.module.scss"
+
+class ActiveQuiz extends Component {
+
+	render() {
+		return (
+			<div className={classes.ActiveQuiz}>
+				<h1>ActiveQuiz</h1>
+			</div>
+		)
+	}
+}
+
+export default ActiveQuiz;
