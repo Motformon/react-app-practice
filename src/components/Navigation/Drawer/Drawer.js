ff8646d0c8d9c22ba9fe5d35e69de278563bd4af
Motformon <Motformon@gmail.com>
2018-10-16 09:03:25 +0300

    Настройка react-router

--- src/components/Navigation/Drawer/Drawer.js
+++ src/components/Navigation/Drawer/Drawer.js
@@ -1,12 +1,19 @@
 import React, {Component} from "react";
 import classes from "./Drawer.module.scss"
 import Backdrop from "../../UI/Backdrop/Backdrop"
+import {NavLink} from "react-router-dom";
 
 const links = [
-	1, 2, 3
+	{to: '/',	label: 'Список', exact: true},
+	{to: '/auth',	label: 'Авторизация', exact: false},
+	{to: '/quiz-creator',	label: 'Создать тест', exact: false},
 ]
 
 class Drawer extends Component {
+
+	clickHandler = () => {
+		this.props.onClose();
+	}
 
 	renderLinks() {
 		return links.map((link, index) => {
@@ -14,7 +21,14 @@ class Drawer extends Component {
 				<li
 					key={index}
 				>
-					<a>Link {link}</a>
+				<NavLink
+					to={link.to}
+					exact={link.exact}
+					activeClassName={classes.active}
+					onClick={this.clickHandler}
+				>
+					{link.label}
+				</NavLink>
 				</li>
 			)
 		})
