ad780085ae8f3f358bae1de51f64334743662057
Motformon <Motformon@gmail.com>
2018-10-17 08:58:42 +0300

    Разметка и стилизация формы

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,12 +1,52 @@
 import React, { Component } from 'react';
-
-
+import classes from "./Auth.module.scss";
+import Button from "../../components/UI/Button/Button"
+import Input from "../../components/UI/Input/Input"
 
 class Auth extends Component {
+
+	loginHandler = () => {
+
+	}
+
+	registerHandler = () => {
+
+	}
+
+	submitHandler = (event) => {
+		event.preventDefault();
+	}
+
   render() {
     return (
-      <div>
-				<h1>Auth</h1>
+      <div className={classes.Auth}>
+				<div>
+					<h1>Auth</h1>
+					<form className={classes.Auth__form} action="" onSubmit={this.submitHandler}>
+						<Input
+							label={'Email'}
+						/>
+						<Input
+							label={'Пароль'}
+							errorMessage={'test'}
+						/>
+
+						<Button 
+							type='success'
+							onClick={this.loginHandler}
+						>
+							Войти
+						</Button>
+
+						<Button 
+							type='primary'
+							onClick={this.registerHandler}
+						>
+							Зарегистрироваться 
+						</Button>
+
+					</form>
+				</div>
       </div>
     );
   }
