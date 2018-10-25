790f53c00ef0b922f911acfe1911e7ee1516da0c
Motformon <Motformon@gmail.com>
2018-10-25 08:32:20 +0300

    Начальная настройка авторизации

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,7 +3,7 @@ import classes from "./Auth.module.scss";
 import Button from "../../components/UI/Button/Button";
 import Input from "../../components/UI/Input/Input";
 import is from "is_js";
-
+import axios from 'axios';
 
 class Auth extends Component {
 
@@ -37,12 +37,32 @@ class Auth extends Component {
 		}
 	}
 
-	loginHandler = () => {
-
+	loginHandler = async () => {
+		const authData = {
+			email: this.state.formControls.email.value,
+			password: this.state.formControls.password.value,
+			returnSecureToken: true
+		}
+		try {
+			const response = await	axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyAy6CElcTRaH59WAu14m5oJf-UJhadEcDA', authData);
+			console.log(response);
+		} catch(e) {
+			console.log(e);
+		}
 	}
 
-	registerHandler = () => {
-
+	registerHandler = async () => {
+		const authData = {
+			email: this.state.formControls.email.value,
+			password: this.state.formControls.password.value,
+			returnSecureToken: true
+		}
+		try {
+			const response = await	axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyAy6CElcTRaH59WAu14m5oJf-UJhadEcDA', authData);
+			console.log(response);
+		} catch(e) {
+			console.log(e);
+		}
 	}
 
 	submitHandler = (event) => {
